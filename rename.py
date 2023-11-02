78b79f5c3aebbf598dad4b55e0fc60b1feb4a651
Sourcery AI <>
2023-11-02 14:08:02 +0000

    'Refactored by Sourcery'

--- rename.py
+++ rename.py
@@ -1,16 +1,13 @@
 newName = input("[+] enter the new name : ")
-newName = '"'+newName+'"'
-file  = open('/data/data/com.termux/files/home/.zshrc','r')
-allLines = []
-i = 0;pos= 0
-for line in file.readlines():
-    if "TNAME=" in line:
-        pos = i
-    allLines.append(line)
-    i+=1
-allLines[pos]="TNAME="+newName+"\n"
-file.close()
-file = open('/data/data/com.termux/files/home/.zshrc','w')
-file.writelines(allLines)
-file.close()
+newName = f'"{newName}"'
+with open('/data/data/com.termux/files/home/.zshrc','r') as file:
+    allLines = []
+    pos= 0
+    for i, line in enumerate(file):
+        if "TNAME=" in line:
+            pos = i
+        allLines.append(line)
+    allLines[pos] = f"TNAME={newName}" + "\n"
+with open('/data/data/com.termux/files/home/.zshrc','w') as file:
+    file.writelines(allLines)
 print("Updated sucessfully restart to see changes")
